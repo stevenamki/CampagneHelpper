dcd7f1e56adb24191a847896729c82e29fd35c7c
Steven <stevenamki@gmail.com>
2023-05-08 17:26:57 -0400

    add adding character view

--- WinFormsApp1/View/Character/AddCharater.Designer.cs
+++ WinFormsApp1/View/Character/AddCharater.Designer.cs
@@ -28,12 +28,115 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+			this.listView2 = new System.Windows.Forms.ListView();
+			this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+			this.BtnCancel = new System.Windows.Forms.Button();
+			this.BtnCreate = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Location = new System.Drawing.Point(12, 57);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(253, 335);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "groupBox1";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(12, 24);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(38, 15);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "label1";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(56, 21);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(209, 23);
+			this.textBox1.TabIndex = 2;
+			// 
+			// listView1
+			// 
+			this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+			this.listView1.Location = new System.Drawing.Point(271, 66);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(517, 146);
+			this.listView1.TabIndex = 3;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			// 
+			// listView2
+			// 
+			this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader3,
+            this.columnHeader4});
+			this.listView2.Location = new System.Drawing.Point(271, 245);
+			this.listView2.Name = "listView2";
+			this.listView2.Size = new System.Drawing.Size(517, 147);
+			this.listView2.TabIndex = 4;
+			this.listView2.UseCompatibleStateImageBehavior = false;
+			// 
+			// BtnCancel
+			// 
+			this.BtnCancel.Location = new System.Drawing.Point(632, 415);
+			this.BtnCancel.Name = "BtnCancel";
+			this.BtnCancel.Size = new System.Drawing.Size(75, 23);
+			this.BtnCancel.TabIndex = 7;
+			this.BtnCancel.Text = "Cancel";
+			this.BtnCancel.UseVisualStyleBackColor = true;
+			// 
+			// BtnCreate
+			// 
+			this.BtnCreate.Location = new System.Drawing.Point(713, 415);
+			this.BtnCreate.Name = "BtnCreate";
+			this.BtnCreate.Size = new System.Drawing.Size(75, 23);
+			this.BtnCreate.TabIndex = 6;
+			this.BtnCreate.Text = "Create";
+			this.BtnCreate.UseVisualStyleBackColor = true;
+			// 
+			// AddCharater
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.BtnCancel);
+			this.Controls.Add(this.BtnCreate);
+			this.Controls.Add(this.listView2);
+			this.Controls.Add(this.listView1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.groupBox1);
+			this.Name = "AddCharater";
 			this.Text = "AddCharater";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private GroupBox groupBox1;
+		private Label label1;
+		private TextBox textBox1;
+		private ListView listView1;
+		private ColumnHeader columnHeader1;
+		private ColumnHeader columnHeader2;
+		private ListView listView2;
+		private ColumnHeader columnHeader3;
+		private ColumnHeader columnHeader4;
+		private Button BtnCancel;
+		private Button BtnCreate;
 	}
 }
